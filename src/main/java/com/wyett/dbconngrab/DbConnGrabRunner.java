5822774a37bce147eba2fd37f221b6d834da1f59
wyettlei@hotmail.com <wyettlei@hotmail.com>
2019-10-12 19:28:54 +0800

    grab db conn

---
+++ src/main/java/com/wyett/dbconngrab/DbConnGrabRunner.java
@@ -0,0 +1,11 @@
+package com.wyett.dbconngrab;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2019/10/10 12:05
+ * @description: TODO
+ */
+
+public class DbConnGrabRunner {
+
+}
---
+++ src/main/java/com/wyett/dbconngrab/accepter/DbConnGrabAccepter.java
@@ -0,0 +1,23 @@
+package com.wyett.dbconngrab.accepter;
+
+import io.netty.channel.EventLoopGroup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2019/10/10 12:15
+ * @description: TODO
+ */
+
+public class DbConnGrabAccepter implements Runnable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DbConnGrabAccepter.class);
+
+    EventLoopGroup
+
+    public void run() {
+
+    }
+}
